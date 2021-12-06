83253f214352d4e8e56138105ada8e4bef816fc5
Matthew Mercieca <mercieca@gmail.com>
2021-12-06 12:00:59 -0500

    Initial port

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { GameBoardComponent } from './game-board/game-board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    HomepageComponent,
+    NavBarComponent,
+    GameBoardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/game-board/game-board.component.ts
@@ -0,0 +1,118 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-game-board',
+  templateUrl: './game-board.component.html',
+  styleUrls: ['./game-board.component.sass']
+})
+
+export class GameBoardComponent implements OnInit {
+  board: boolean[][];
+  size: number;
+  moves: number;
+  boardSize: Array<number>;
+  subscrption: any;
+
+  constructor(private router: Router, private route: ActivatedRoute) { 
+    this.boardSize = new Array<number>();
+    this.size = 0;
+    this.moves = 0;
+    this.board = [];
+  }
+
+  ngOnInit() {
+    this.subscrption = this.route.params.subscribe(params => {
+      this.size = +params['size'];
+      this.moves = +params['moves'];
+  
+      this.boardSize = new Array<number>();
+      for (let i = 0; i < this.size; i++) {
+        this.boardSize[i] = i;
+      }
+
+      this.reset();
+    })
+  }
+
+  ngOnDestroy() {
+    this.subscrption.unsubscribe();
+  }
+
+  classesFor(row:any, col:any) {
+    let classes = [];
+    if (this.size == 7) {
+      classes.push('seven');
+    }
+
+    if (this.board[row][col]) {
+      classes.push('on');
+    } else {
+      classes.push('off');
+    }
+
+    return classes;
+  }
+
+  get isWon() {
+    let isWon = true;
+    for (let row = 0; row < this.size; row++) {
+      for (let col = 0; col < this.size; col++) {
+        isWon = isWon && this.board[row][col];
+
+        if (!isWon) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  toggle(row:any, col:any) {
+    if (row >= 0 && col >= 0 && row < this.size && col < this.size) {
+      this.board[row][col] = !this.board[row][col];
+    }
+  }
+
+  switch(row:any, col:any) {
+    this.toggle(row, col);
+    this.toggle(row - 1, col);
+    this.toggle(row + 1, col);
+    this.toggle(row, col - 1);
+    this.toggle(row, col + 1);
+  }
+
+  onPress(row:any, col:any) {
+    this.switch(row, col);
+  }
+
+  goHome() {
+    this.router.navigateByUrl('/home');
+  }
+
+  randomMove() {
+    let row = Math.floor(Math.random() * this.size);
+    let col = Math.floor(Math.random() * this.size);
+    
+    this.switch(row, col);
+  }
+
+  reset() {
+    this.board = [];
+    for (let row = 0; row < this.size; row++) {
+      this.board[row] = [];
+      for (let col = 0; col < this.size; col++) {
+        this.board[row][col] = true;
+      }
+    }
+
+    for (let i = 0; i < this.moves; i++) {
+      this.randomMove();
+
+      if (this.isWon) {
+        this.randomMove();
+      }
+    }
+  }
+}
